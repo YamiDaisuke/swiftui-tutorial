6e62d76a094b2f23d691fc36b9568d5049db7d22
Franklin Cruz <fcruz@smartboxtv.com>
2019-11-04 17:53:05 -0300

    Creating and combining views

---
+++ landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Franklin Cruz on 4/11/19.
+//  Copyright © 2019 S.Y.Soft. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
