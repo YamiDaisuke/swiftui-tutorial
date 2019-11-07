07864010236ab50c3e7c501351ad0bc045dee265
Franklin Cruz <fcruz@smartboxtv.com>
2019-11-07 14:20:33 -0300

    Handling User Input

---
+++ landmarks/Models/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  landmarks
+//
+//  Created by Franklin Cruz on 5/11/19.
+//  Copyright © 2019 S.Y.Soft. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+final class UserData: ObservableObject  {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
